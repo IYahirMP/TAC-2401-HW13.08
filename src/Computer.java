599d0f82a1dbe58c9ebdad1f5275812732438074
IYahirMP <ivanyahirmopi@gmail.com>
2024-08-13 00:58:28 -0600

    Comments and constructors added

--- src/Computer.java
+++ src/Computer.java
@@ -1,2 +1,176 @@
-package PACKAGE_NAME;public class Computer {
+/**
+ * The class Computer represents a physical computer that is subject
+ * to computer repair services. This class holds information regarding
+ * the computer's serial number, brand, model, production year, and whether
+ * it is a custom build.
+ * The class provides methods to manage computer data.
+ *
+ * @version 1.0 12 Aug 2024
+ * @author Ivan Mojica
+ */
+public class Computer {
+
+    /** Holds the computer's id for the repair service */
+    private int computerId;
+
+    /** Holds the computer's serial number if available. */
+    private String serialNumber;
+
+    /** Holds the computer's brand if available. */
+    private String brand;
+
+    /** Holds the computer's model if available. */
+    private String model;
+
+    /** Holds the computer's year of production. */
+    private int year;
+
+    /** Indicates if the computer is a custom build. */
+    private boolean isCustomAssembled;
+
+    /**
+     * The default constructor for Computer.
+     * Initializes serialNumber, brand and model to an empty string,
+     * year to 0, id to -1, and isCustomAssembled to false.
+     */
+    public Computer() {
+        this.computerId = -1;
+        this.serialNumber = "";
+        this.brand = "";
+        this.model = "";
+        this.year = 0;
+        this.isCustomAssembled = false;
+    }
+
+    /**
+     * Custom constructor for Computer.
+     * Initializes serialNumber, brand, model, year and isCustomAssembled
+     * to their specified values.
+     * @param computerId The computer's assigned id for the computer repair service
+     * @param serialNumber The computer's serial number.
+     * @param brand The computer's brand.
+     * @param model The computer's model.
+     * @param year The computer's production year
+     * @param isCustomAssembled Whether the computer was a custom assembly or not.
+     */
+    public Computer(int computerId, String serialNumber, String brand, String model, int year, boolean isCustomAssembled) {
+        this.computerId = computerId;
+        this.serialNumber = serialNumber;
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.isCustomAssembled = isCustomAssembled;
+    }
+
+    /**
+     * Custom constructor for Computer intended for computers not built by any trademark
+     * Initializes serialNumber, brand and model to "N/A", isCustomAssembled is set to true,
+     * and assigns the given computerId and year.
+     * @param computerId The new computer's id for the computer repair service.
+     * @param year The new computer's year of assembly.
+     */
+    public Computer(int computerId, int year) {
+        this.computerId = computerId;
+        this.serialNumber = "N/A";
+        this.brand = "N/A";
+        this.model = "N/A";
+        this.year = year;
+        this.isCustomAssembled = true;
+    }
+
+    /**
+     * Retrieves the computer's computer repair service id.
+     * @return computer's id for the computer repair service.
+     */
+    public int getComputerId() {
+        return computerId;
+    }
+
+    /**
+     * Sets the computer's id for the computer repair service.
+     * @param computerId The new computer's id for the computer repair service.
+     */
+    public void setComputerId(int computerId) {
+        this.computerId = computerId;
+    }
+
+    /**
+     * Retrieves the serial number of the computer.
+     * @return The serialNumber of the Computer.
+     */
+    public String getSerialNumber() {
+        return this.serialNumber;
+    }
+
+    /**
+     * Sets the serial number of the computer.
+     * @param serialNumber The serial number to set for the Computer.
+     */
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    /**
+     * Retrieves the brand of the computer.
+     * @return The brand of the Computer.
+     */
+    public String getBrand() {
+        return this.brand;
+    }
+
+    /**
+     * Sets the brand of the computer.
+     * @param brand The brand to set for the Computer.
+     */
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    /**
+     * Retrieves the model of the computer.
+     * @return The model of the Computer.
+     */
+    public String getModel() {
+        return this.model;
+    }
+
+    /**
+     * Sets the model of the computer.
+     * @param model The model to set for the Computer.
+     */
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    /**
+     * Retrieves the year of production of the computer.
+     * @return The year of production of the Computer.
+     */
+    public int getYear() {
+        return this.year;
+    }
+
+    /**
+     * Sets the year of production of the computer.
+     * @param year The year to set for the Computer.
+     */
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    /**
+     * Checks if the computer is a custom build.
+     * @return True if the Computer is custom assembled, false otherwise.
+     */
+    public boolean isCustomAssembled() {
+        return this.isCustomAssembled;
+    }
+
+    /**
+     * Sets whether the computer is a custom assembly.
+     * @param isCustomAssembled True if the Computer is custom assembled, false otherwise.
+     */
+    public void setCustomAssembled(boolean isCustomAssembled) {
+        this.isCustomAssembled = isCustomAssembled;
+    }
 }
--- src/ServiceRequest.java
+++ src/ServiceRequest.java
@@ -1,2 +1,130 @@
-package PACKAGE_NAME;public class ServiceRequest {
+import java.time.LocalDate;
+
+/**
+ * The class ServiceRequest represents a service request of the computer
+ * repair service. It holds information such as the request id, request date,
+ * status, and a brief description of the requested services.
+ * The class provides methods to manage service request data;
+ *
+ * @version 1.0 12 Aug 2024
+ * @author Ivan Mojica
+ */
+public class ServiceRequest {
+
+    /** Holds the service request id. */
+    private int requestId;
+
+    /** Holds the date services were requested*/
+    private LocalDate requestDate;
+
+    /** Holds the status of the request */
+    private String status;
+
+    /** Holds a brief description of the request */
+    private String description;
+
+    /**
+     * Default constructor for ServiceRequest.
+     * It initializes requestId as -1, requestDate as today's date,
+     * status and description as empty strings.
+     */
+    public ServiceRequest() {
+        this.requestId = 0;
+        this.requestDate = LocalDate.now();
+        this.status = "";
+        this.description = "";
+    }
+
+    /**
+     * Constructs a ServiceRequest setting custom values for requestId,
+     * requestDate, status and description.
+     * @param requestId Request id for the new ServiceRequest.
+     * @param requestDate Request date for the new ServiceRequest.
+     * @param status Current status for the new ServiceRequest.
+     * @param description Brief description of the requested services in the new ServiceRequest.
+     */
+    public ServiceRequest(int requestId, LocalDate requestDate, String status, String description) {
+        this.requestId = requestId;
+        this.requestDate = requestDate;
+        this.status = status;
+        this.description = description;
+    }
+
+    /**
+     * Constructs a ServiceRequest using only a requestId, status and description.
+     * requestDate is set as current date.
+     * @param requestId Request id for the new ServiceRequest.
+     * @param status Current status for the new ServiceRequest.
+     * @param description Brief description of the requested services in the new ServiceRequest.
+     */
+    public ServiceRequest(int requestId, String status, String description) {
+        this.requestId = requestId;
+        this.requestDate = LocalDate.now();
+        this.status = status;
+        this.description = description;
+    }
+
+    /**
+     * Retrieves the id of the service request.
+     * @return The requestId associated with the ServiceRequest.
+     */
+    public int getRequestId() {
+        return requestId;
+    }
+
+    /**
+     * Updates the current requestId with the provided value.
+     * @param requestId The requestId to set for the ServiceRequest.
+     */
+    public void setRequestId(int requestId) {
+        this.requestId = requestId;
+    }
+
+    /**
+     * Retrieves the request date of the service request.
+     * @return The request date of the ServiceRequest.
+     */
+    public LocalDate getRequestDate() {
+        return this.requestDate;
+    }
+
+    /**
+     * Updates the request date with the provided date.
+     * @param requestDate The new date for the ServiceRequest.
+     */
+    public void setRequestDate(LocalDate requestDate) {
+        this.requestDate = requestDate;
+    }
+
+    /**
+     * Retrieves the current status of the service request.
+     * @return The current status of the ServiceRequest.
+     */
+    public String getStatus() {
+        return this.status;
+    }
+
+    /**
+     * Updates the status of the service request with the provided value.
+     * @param status The new status for the ServiceRequest.
+     */
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    /**
+     * Retrieves a brief description of the requested services.
+     * @return The description of the ServiceRequest.
+     */
+    public String getDescription() {
+        return this.description;
+    }
+
+    /**
+     * Updates the description of the requested services with the new description.
+     * @param description New description for the ServiceRequest.
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
